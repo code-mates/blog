87bf39fdf62c8bd66457235303836f7ad116ef7e
T.R. Austin <djresonance@gmail.com>
2017-09-05 09:04:12 -0500

    initial code for login (from http://www.phpclasses.org/blog/package/10087/post/1-secure-login-and-registration-system.html)

--- login.php
+++ login.php
@@ -1,1 +1,13 @@
-hi
+<?php
+	require_once '../class/user/php';
+	require_once 'config.php';
+
+	$user->indexHead();
+	$user->indexTop();
+	$user->loginForm;
+	$user->activationForm();
+	$user->indexMiddle();
+	$user->registerForm();
+	$user->indexFooter();
+?>
+
