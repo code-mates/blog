528965d9b73c23fa27364395bfa40102d85f2ee6
Barnato <31374668+Barnato@users.noreply.github.com>
2017-09-05 22:44:01 -0500

    - made mistake - Created database cm_blog when this was previosusly created on Saturday - so removed "CREATE DATABASE cm_blog;"  and just had code to create table article with columns and data types

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-create database cm_blog;
-  
+CREATE TABLE cm_blog.article
+(article_id int NOT NULL AUTO_INCREMENT Primary Key, author_user_id int NOT NULL AUTO_INCREMENT Primary Key, title varchar(35), body text, publish_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
