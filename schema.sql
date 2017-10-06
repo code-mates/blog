1af6fd5c94dfca1fba2ce50236775a1f008375c4
Barnato <31374668+Barnato@users.noreply.github.com>
2017-10-05 20:33:45 -0500

    Database cm_blog with two (2) tables called user table and article table. User table uses MD5 to prevent passwords being shown in plain text

--- schema.sql
+++ schema.sql
@@ -2,4 +2,11 @@ CREATE database cm_blog;
 USE cm_blog;
 SHOW databases;
 CREATE TABLE cm_blog.article
- (article_id int NOT NULL AUTO_INCREMENT Primary Key, author_user_id int NOT NULL, title varchar(35), body text, publish_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+(article_id int NOT NULL AUTO_INCREMENT Primary Key, author_user_id int NOT NULL, title varchar(35), body text, publish_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE cm_blog.user
+(user_id int NOT NULL AUTO_INCREMENT Primary Key, name varchar(35), emailaddress varchar(80), password varchar (32));
+UPDATE user
+SET password = MD5(password);
+INSERT INTO user (password)
+VALUES (MD5('verysecretpassword'));
+SELECT * FROM user WHERE password=MD5('verysecretpassword');
